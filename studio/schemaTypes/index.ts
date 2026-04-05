af2b15c04263a8cb00cdf30ed231d9ee08cb5817
Simon Blurton <sblurton@hotmail.co.uk>
2026-04-05 22:22:34 +0100

    Add color token schema and integrate into schema types

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,15 +1,21 @@
+import {colorTokenType} from './colorToken'
 import {imageSectionType} from './imageSection'
 import {pageType} from './page'
 import {productCardType} from './productCard'
 import {productsSectionType} from './productsSection'
+import {socialLinkType} from './socialLink'
+import {socialLinksSectionType} from './socialLinksSection'
 import {siteSettingsType} from './siteSettings'
 import {textSectionType} from './textSection'
 
 export const schemaTypes = [
+	colorTokenType,
 	pageType,
 	imageSectionType,
 	productCardType,
 	productsSectionType,
+	socialLinkType,
+	socialLinksSectionType,
 	siteSettingsType,
 	textSectionType,
 ]
