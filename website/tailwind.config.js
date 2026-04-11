f0d8de281c8f095f4078c53fef08a758202fbce3
Simon Blurton <sblurton@hotmail.co.uk>
2026-04-11 16:34:03 +0100

    chore: remove stylelint configuration and clear safelist in tailwind config

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,js,ts,jsx,tsx,css,md}', './public/**/*.html'],
-  safelist: [
-    'bg-base-200/10',
-    'bg-base-200/20',
-    'bg-base-200/30',
-    'bg-base-200/40',
-    'bg-base-200/50',
-    'bg-base-200/60',
-    'bg-base-200/70',
-    'bg-base-200/80',
-  ],
+  safelist: [],
   theme: {
     extend: {},
   },
